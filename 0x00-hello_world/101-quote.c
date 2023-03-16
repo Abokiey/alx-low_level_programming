a160d042ead803543bd1447d2960f205649bbdf6
Abokiey <sweneysam@gmail.com>
2023-03-17 01:30:01 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function is used to print statements
+ * Return: should print 1 if code is succesful
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
