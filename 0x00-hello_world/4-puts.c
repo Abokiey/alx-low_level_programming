cb69f55eea243dcee71b9879cb5c31d67c6fd925
Abokiey <sweneysam@gmail.com>
2023-03-16 19:43:36 +0300

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A prigram to help print a line with put function
+ * Return: help return 0 hence code is succesful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
