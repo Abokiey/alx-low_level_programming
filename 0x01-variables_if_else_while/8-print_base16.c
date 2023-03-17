add7118bb9aa32b1bf704d08d27ef289ec2744a6
Abokiey <sweneysam@gmail.com>
2023-03-18 02:10:31 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point for code
+ * main - main function for executing a program
  *
  * Return: always 0
  */
@@ -9,8 +9,11 @@
 int main(void)
 {
 int i;
+char j;
 for (i = 0 ; i < 10 ; i++)
 putchar(i + '0');
+for (j = 'a' ; j <= 'f' ; j++)
+putchar(j);
 putchar('\n');
 return (0);
 }
