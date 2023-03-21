4dec1189a93ae0de2c9146ce7e7995dc88439896
Abokiey <sweneysam@gmail.com>
2023-03-21 18:29:15 +0300

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_sign - prints signs of number
+ * Return: return 1, 0, -1 if conditions are met
+ * @n: guven global variable
+ */
+
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+	
+	return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	return (0);
+	}	
+
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+	return (-1);
+	}
+
+
+}
