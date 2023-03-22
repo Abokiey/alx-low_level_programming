3d243fdcb764491e73e24fbe6c8e56389066e655
Abokiey <sweneysam@gmail.com>
2023-03-22 16:49:28 +0300

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative - show whether a number is positive or negative
+ * @i: global variable
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+
+
+}
