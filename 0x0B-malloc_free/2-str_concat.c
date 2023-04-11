6db482029cc7fd58c0d1f086cf95d4ab3e0d9247
Abokiey <sweneysam@gmail.com>
2023-04-11 17:16:31 +0300

    concatinate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (concat == NULL)
 		return (NULL);
-
+	i = j = 0;
 
 	while (s1[i] != '\0')
 	{
