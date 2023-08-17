7b377173523e4c4381f87bf496b27a8a58ab37c1
Abokiey <sweneysam@gmail.com>
2023-08-17 17:15:27 +0300

    sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
  * sum_dlistint - sum of the whole list
  * @head: pointer to the first ode
  *
- * Retur: the sum of the list
+ * Return: the sum of the list
  */
 
 int sum_dlistint(dlistint_t *head)
