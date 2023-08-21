e128b3c7685e7eb954baad7e8b854f2c79c3707e
Abokiey <sweneysam@gmail.com>
2023-08-21 14:30:45 +0300

    operations

---
+++ 0x18-dynamic_libraries/100-calc.c
@@ -0,0 +1,60 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - adds a and b
+ * @a: a
+ * @b: b
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts a and b
+ * @a: a
+ * @b: b
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies a and b
+ * @a: a
+ * @b: b
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides a and b
+ * @a: a
+ * @b: b
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of a and b
+ * @a: a
+ * @b: b
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
